488e6558f41d81434e6bd315950b696f037c00c6
Vips <vips@home-mac>
2025-05-01 13:57:24 +0530

    Updated Main because deployment was failing

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 # main.py
+import os  # <-- Import the os module
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -7,4 +9,9 @@ def hello():
     return "Hello from GCP CI/CD!"
 
 if __name__ == '__main__':
-    app.run()
+    # Get port from environment variable PORT, default to 8080
+    port = int(os.environ.get("PORT", 8080))
+
+    # Run the app listening on all network interfaces (0.0.0.0)
+    # and on the determined port (required for Cloud Run)
+    app.run(host='0.0.0.0', port=port)
