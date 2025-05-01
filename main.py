f20f5b7bc7ec75093fab53f40f9361330e1d4271
Vips <vips@home-mac>
2025-05-01 13:35:14 +0530

    Initial CI/CD test push

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
+# main.py
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello from GCP CI/CD pipeline!"
+    return "Hello from GCP CI/CD!"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    app.run()
